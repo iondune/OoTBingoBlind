b25b9284cbf9a30bafff8398d3c60b9f5c359678
Ian Dunn <iondune@gmail.com>
2017-05-29 18:46:39 -0400

    Implement server-side storage of squares

--- routes/rooms.js
+++ routes/rooms.js
@@ -5,14 +5,15 @@ var crypto = require('crypto');
 var bs58 = require('bs58')
 
 
-var numRenders = 0;
+var rooms = {};
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   var roomId = bs58.encode(crypto.randomBytes(3*4));
   console.log("Made room with id %s", roomId);
-  console.log("req.originalUrl %s", req.originalUrl)
-  res.redirect(req.originalUrl + '/' + roomId)
+  console.log("req.originalUrl %s", req.originalUrl);
+
+  res.redirect(req.originalUrl + '/' + roomId);
 });
 
 /* GET users listing. */
@@ -20,24 +21,49 @@ router.get('/:roomId', function(req, res, next) {
   var roomId = req.params.roomId;
 
   console.log("serving room %s", roomId);
-  numRenders = numRenders + 1;
-  console.log("rendered %d times", numRenders);
+
+  if (! rooms.hasOwnProperty(roomId)) {
+    rooms[roomId] = {};
+  }
 
   var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
   console.log("fullUrl: %s", fullUrl)
 
   res.render('room', {
     title: 'Room ' + roomId,
-    numRenders: numRenders,
     roomId: roomId,
     roomUrl: fullUrl
   });
 });
 
+router.get('/:roomId/list', function(req, res, next) {
+  var roomId = req.params.roomId;
+  var team = req.query.team;
+  console.log("Got a list for room: %s team: %s", roomId, team);
+
+  var response = {};
+  response["squares"] = [];
+
+  // console.log("room: %O", rooms[roomId]);
+
+  for (var square in rooms[roomId]) {
+    if (rooms[roomId][square] == team) {
+      response["squares"].push(square);
+    }
+    // console.log("square %s owner %s", square, rooms[roomId][square]);
+  }
+  // console.log("response: %O", response);
+
+  res.json(response);
+});
+
 router.post('/:roomId/square', function(req, res) {
   var roomId = req.params.roomId;
+  var square = req.body.id;
+  var team = req.body.team;
 
-  console.log("Got a square req for room: %s team: %s id: %s", roomId, req.body.team, req.body.id);
+  console.log("Got a square req for room: %s team: %s id: %s", roomId, team, square);
+  rooms[roomId][square] = team;
   res.sendStatus(200);
 });
 
