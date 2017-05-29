044cdaaba707635ffdbc60e79b73014e3b4e3021
Ian Dunn <iondune@gmail.com>
2017-05-29 16:29:28 -0400

    Add randomly generated rooms

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var crypto = require('crypto');
+var bs58 = require('bs58')
+
+
 var numRenders = 0;
 
 /* GET users listing. */
+router.get('/', function(req, res, next) {
+  var roomId = bs58.encode(crypto.randomBytes(3*4));
+  res.redirect(roomId)
+});
+
+/* GET users listing. */
 router.get('/:roomId', function(req, res, next) {
-  console.log("serving room %s", req.params.roomId);
+  var roomId = req.params.roomId;
+
+  console.log("serving room %s", roomId);
   numRenders = numRenders + 1;
   console.log("rendered %d times", numRenders);
 
-  res.render('room', { title: 'Room ' + req.params.roomId, numRenders: numRenders });
+  res.render('room', { title: 'Room ' + roomId, numRenders: numRenders, roomId: roomId });
 });
 
 module.exports = router;
