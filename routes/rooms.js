84bbc7fea9be5c3596a8ea6a8df365bcf5235d7c
Ian Dunn <iondune@gmail.com>
2017-05-29 23:01:05 -0400

    Setting a square doesn't unset it for other players

--- routes/rooms.js
+++ routes/rooms.js
@@ -46,13 +46,13 @@ router.get('/:roomId/list', function(req, res, next) {
 
   // console.log("room: %O", rooms[roomId]);
 
-  for (var square in rooms[roomId]) {
-    if (rooms[roomId][square] == team) {
-      response["squares"].push(square);
+  if (Array.isArray(rooms[roomId][team])) {
+    for (var i = 0; i < rooms[roomId][team].length; ++ i) {
+      response["squares"].push(rooms[roomId][team][i]);
     }
-    // console.log("square %s owner %s", square, rooms[roomId][square]);
+
+    console.log("%O", rooms[roomId][team]);
   }
-  // console.log("response: %O", response);
 
   res.json(response);
 });
@@ -61,9 +61,21 @@ router.post('/:roomId/square', function(req, res) {
   var roomId = req.params.roomId;
   var square = req.body.id;
   var team = req.body.team;
+  var event = req.body.event;
 
-  console.log("Got a square req for room: %s team: %s id: %s", roomId, team, square);
-  rooms[roomId][square] = team;
+  console.log("Got a square req for room: %s team: %s id: %s event: %s", roomId, team, square);
+  if (! Array.isArray(rooms[roomId][team])) {
+    rooms[roomId][team] = [];
+  }
+
+  if (event == "click") {
+    rooms[roomId][team].push(square);
+  }
+  else if (event == "unclick") {
+    rooms[roomId][team] = rooms[roomId][team].filter(function(arg) { return arg != square; });
+  }
+  console.log("%O", rooms[roomId][team]);
+
   res.sendStatus(200);
 });
 
